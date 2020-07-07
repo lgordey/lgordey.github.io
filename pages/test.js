ecf886e5a4d292e105441e4b82288d319f7d10d4
Gordey Levchenko <levchenko.gordey@gmail.com>
2020-07-07 12:56:57 +0300

    Add btns getMe and getPhone

--- pages/test.js
+++ pages/test.js
@@ -2,55 +2,43 @@ import React, { useState, useEffect } from 'react';
 import withStyles from 'react-jss';
 import aituBridge from '@btsd/aitu-bridge';
 
-const eventType = 'aituEvents';
-
 const styles = {
   testWrapper: {
     width: '100%',
     maxWidth: 800,
     margin: '0 auto'
+  },
+  btnWrapper: {
+    padding: 20,
+    border: '2px solid black',
+    marginTop: 20,
+    '& button:not(:first-child)': {
+      marginLeft: 20
+    }
   }
 };
 
 function Test({ classes }) {
   const [calledMethods, setCalledMethods] = useState([]);
-  const [methodName, setMethodName] = useState('');
   const [receivedData, setReceivedData] = useState([]);
 
   useEffect(() => {
-    invokeMethod('InitApps');
-    // window.addEventListener(eventType, (event) => {
-    //   const evStr = JSON.stringify(event.detail);
-    //   console.log('eventType', eventType);
-    //   console.log('event', event);
-    //   setReceivedData(prevArray => [...prevArray, evStr])
-    // });
+    handleMethod('InitApps');
   }, []);
 
-  const handleChange = (event) => {
-    setMethodName(event.target.value);
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    invokeMethod(methodName);
-  }
-
-  const invokeMethod = (name) => {
-    aituBridge.invoke(name);
-    setCalledMethods(prevArray => [...prevArray, name])
-    setMethodName('');
+  const handleMethod = async (methodName) => {
+    setCalledMethods(prevArray => [...prevArray, methodName])
+    const data = await aituBridge.invoke(methodName);
+    console.log('__Received__data!', data);
+    setReceivedData(prevArray => [...prevArray, data])
   }
 
   return (
     <div className={classes.testWrapper}>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Method name:
-          <input type="text" name="method-name" value={methodName} onChange={handleChange} />
-        </label>
-        <input type="submit" value="Invoke method" />
-      </form>
+      <div className={classes.btnWrapper}>
+        <button onClick={() => handleMethod('getMe')}>getMe</button>
+        <button onClick={() => handleMethod('getPhone')}>getPhone</button>
+      </div>
       <div style={{ marginTop: 20, display: 'flex' }}>
         <div style={{ width: '50%' }}>
           <b style={{ marginBottom: 10 }}>Invoked methods:</b>
