a63a0940aeea61a187cbeaa4896b0461a382d088
Gordey Levchenko <levchenko.gordey@gmail.com>
2020-08-25 02:05:14 +0300

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5a230240270916e37d92";
+/******/ 	var hotCurrentHash = "53d5befd8f14da705d64";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
