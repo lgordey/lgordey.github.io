d994041a33785b7937c0168b0f7b5eec8c810410
Gordey Levchenko <gordey.levchenko@btsdigital.kz>
2021-10-07 16:13:09 +0300

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ef350cf02f73bbbc85e4";
+/******/ 	var hotCurrentHash = "63695918fc9298659a41";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/webpack/static/development/pages/test-iframe.js.403bc8445f5d301f353f.hot-update.js
@@ -0,0 +1,58 @@
+webpackHotUpdate("static/development/pages/test-iframe.js",{
+
+/***/ "./pages/test-iframe.js":
+/*!******************************!*\
+  !*** ./pages/test-iframe.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_jss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-jss */ "./node_modules/react-jss/dist/react-jss.esm.js");
+/* harmony import */ var _basedImages__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../basedImages */ "./basedImages/index.js");
+var _jsxFileName = "/Users/lgordey/BTS/lgordey.github.io/pages/test-iframe.js";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+var styles = {
+  testWrapper: {
+    width: '100%',
+    maxWidth: 800,
+    margin: '0 auto'
+  }
+};
+
+function Test(_ref) {
+  var classes = _ref.classes;
+  return __jsx("div", {
+    className: classes.testWrapper,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    },
+    __self: this
+  }, __jsx("iframe", {
+    id: "aitu_messenger_frame",
+    width: "940",
+    height: "750",
+    src: "http://localhost:3000/?theme=light&lang=ru&kuserid=1000003292029",
+    frameBorder: "0",
+    allowFullScreen: "",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17
+    },
+    __self: this
+  }));
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (Object(react_jss__WEBPACK_IMPORTED_MODULE_1__["default"])(styles)(Test));
+
+/***/ })
+
+})
+//# sourceMappingURL=test-iframe.js.403bc8445f5d301f353f.hot-update.js.map
