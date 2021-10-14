4dfaa26da454510bcaebde78d791a9d05ad650f9
Gordey Levchenko <gordey.levchenko@btsdigital.kz>
2021-10-14 11:44:57 +0300

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "63695918fc9298659a41";
+/******/ 	var hotCurrentHash = "54a5c40e96882401e1f8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
